c6631c95d5ff22505f8aaf1b8cb232411c2fcdb3
Richard Oden <richard.thomas.oden@gmail.com>
2022-06-20 15:49:52 -0400

    In main.py, implemented pandas to parse gun laws table instead of BeautifulSoup.

--- src/main.py
+++ src/main.py
@@ -4,13 +4,8 @@ import os
 import requests
 
 violent_deaths_df = pd.read_excel(os.path.join('data', 'Small-Arms-Survey-DB-violent-deaths.xlsx'), usecols="C, D, AI", skiprows=[0, 1])
+gun_laws_df = pd.read_html('https://en.wikipedia.org/wiki/Overview_of_gun_laws_by_nation', match='Gun laws worldwide')[0]
 
-gun_laws_request = requests.get('https://en.wikipedia.org/wiki/Overview_of_gun_laws_by_nation')
+gun_laws_df.columns = [col[1] for col in gun_laws_df.columns]
 
-if not gun_laws_request.ok:
-    raise RuntimeError('Failed to retrieve gun laws wikipedia article.')
-
-soup = BeautifulSoup(gun_laws_request.content, 'html.parser')
-table = soup.find('caption', text='Gun laws worldwide\n').parent()
-
-print(table)
+print(gun_laws_df[gun_laws_df['Region'].str.contains('Albania')])
