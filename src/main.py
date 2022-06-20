84e4b49b3f520b1d33c52ecb0527fc357ea2d02e
Richard Oden <richard.thomas.oden@gmail.com>
2022-06-20 11:03:37 -0400

    Added code to import gun laws wiki article HTML in main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
+from bs4 import BeautifulSoup
 import pandas as pd
 import os
+import requests
 
-df = pd.read_excel(os.path.join('data', 'Small-Arms-Survey-DB-violent-deaths.xlsx'), usecols="C, D, AI", skiprows=[0, 1])
+violent_deaths_df = pd.read_excel(os.path.join('data', 'Small-Arms-Survey-DB-violent-deaths.xlsx'), usecols="C, D, AI", skiprows=[0, 1])
 
-print(df.head())
+gun_laws_request = requests.get('https://en.wikipedia.org/wiki/Overview_of_gun_laws_by_nation')
+
+if not gun_laws_request.ok:
+    raise RuntimeError('Failed to retrieve gun laws wikipedia article.')
+
+soup = BeautifulSoup(gun_laws_request.content, 'html.parser')
+print(soup.prettify())
