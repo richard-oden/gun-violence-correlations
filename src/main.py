5cf7d188997d39213b87e1bf028e39f5d1dbd002
Richard Oden <richard.thomas.oden@gmail.com>
2022-06-25 16:40:06 -0400

    Added GunLawsColumnNames in main.py and began working on calculate_mean_regulation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import enum
 import os
 import pandas as pd
+from statistics import mean
 
 # Create gun deaths dataframe from Small Arms Survey excel document.
 # https://www.smallarmssurvey.org/database/global-violent-deaths-gvd
@@ -18,19 +19,35 @@ gun_laws_df = pd.read_html('https://en.wikipedia.org/wiki/Overview_of_gun_laws_b
 # Convert MultiIndex to single Index.
 gun_laws_df = gun_laws_df.droplevel(level=[0, 2], axis=1)
 
+
+class GunLawsColumnNames(enum.Enum):
+    '''
+    Represents column names in gun_laws_df.
+    '''
+    COUNTRY = 'Country',
+    GOOD_REASON = 'Good Reason Required',
+    PERSONAL_PROTECTION = 'Personal Protection Permitted',
+    LONG_GUNS = 'Long Guns Permitted',
+    HANDGUNS = 'Handguns Permitted',
+    SEMIAUTOMATIC = 'Semi-Automatic Permitted',
+    FULLY_AUTOMATIC = 'Fully Automatic Permitted',
+    OPEN_CARRY = 'Open Carry Permitted',
+    CONCEALED_CARRY = 'Concealed Carry Permitted',
+    FREE_OF_REGISTRATION = 'Free of Registration'
+
 # Drop unwanted columns and rename remaining.
 gun_laws_df.drop(['Magazine capacity limits[N 1]', 'Max penalty (years)[2]'], axis=1, inplace=True)
 gun_laws_df.rename(columns={
-    'Region': 'Country',
-    'Good reason required?[3]': 'Good reason required',
-    'Personal protection': 'Personal protection permitted',
-    'Long guns (exc. semi- and full-auto)[4]': 'Long guns permitted',
-    'Handguns[5]': 'Handguns permitted',
-    'Semi-automatic rifles': 'Semi-automatic permitted',
-    'Fully automatic firearms[6]': 'Fully automatic permitted',
-    'Open carry[7]': 'Open carry permitted',
-    'Concealed carry[8]': 'Concealed carry permitted',
-    'Free of registration[1]': 'Free of registration'
+    'Region': GunLawsColumnNames.COUNTRY.value,
+    'Good reason required?[3]': GunLawsColumnNames.GOOD_REASON.value,
+    'Personal protection': GunLawsColumnNames.PERSONAL_PROTECTION.value,
+    'Long guns (exc. semi- and full-auto)[4]': GunLawsColumnNames.LONG_GUNS.value,
+    'Handguns[5]': GunLawsColumnNames.HANDGUNS.value,
+    'Semi-automatic rifles': GunLawsColumnNames.SEMIAUTOMATIC.value,
+    'Fully automatic firearms[6]': GunLawsColumnNames.FULLY_AUTOMATIC.value,
+    'Open carry[7]': GunLawsColumnNames.OPEN_CARRY.value,
+    'Concealed carry[8]': GunLawsColumnNames.CONCEALED_CARRY.value,
+    'Free of registration[1]': GunLawsColumnNames.FREE_OF_REGISTRATION.value
 }, inplace=True)
 
 # Clean gun laws dataframe.
@@ -42,7 +59,7 @@ class Regulation(enum.Enum):
     MOSTLY_UNREGULATED = 3
     HIGHLY_UNREGULATED = 4
 
-def get_regulation(cell: str, is_restriction: bool) -> int:
+def get_regulation(cell: str, is_restriction: bool) -> Regulation:
     '''
     Given a cell from gun_laws_df and and a bool indicating whether or not this column 
     represents a restriction, returns a value from the Regulation enum.
@@ -75,20 +92,25 @@ def get_regulation(cell: str, is_restriction: bool) -> int:
 
     return Regulation.CONDITIONAL
 
-def convert_to_regulations(row):
+def convert_to_regulations(row: pd.Series) -> None:
     '''
     Converts regulation-related cells in a given row from gun_laws_df to Regulation enum values.
     '''
-    row['Good reason required'] = get_regulation(row['Good reason required'], True)
-    row['Personal protection permitted'] = get_regulation(row['Personal protection permitted'], False)
-    row['Long guns permitted'] = get_regulation(row['Long guns permitted'], False)
-    row['Handguns permitted'] = get_regulation(row['Handguns permitted'], False)
-    row['Semi-automatic permitted'] = get_regulation(row['Semi-automatic permitted'], False)
-    row['Fully automatic permitted'] = get_regulation(row['Fully automatic permitted'], False)
-    row['Open carry permitted'] = get_regulation(row['Open carry permitted'], False)
-    row['Concealed carry permitted'] = get_regulation(row['Concealed carry permitted'], False)
-    row['Free of registration'] = get_regulation(row['Free of registration'], False)
+    for column_name in GunLawsColumnNames:
+        if column_name is GunLawsColumnNames.COUNTRY:
+            continue
+        is_restriction = column_name is GunLawsColumnNames.GOOD_REASON
+        row[column_name.value] = get_regulation(row[column_name.value], is_restriction)
 
 gun_laws_df.apply(convert_to_regulations, axis=1)
+
+def calculate_mean_regulation(row: pd.Series):
+    '''
+    Calculates the mean regulation for row and appends as column.
+    '''
+    row['Summary Regulation'] = mean([row[column_name.value].value for column_name in GunLawsColumnNames 
+        if column_name is not GunLawsColumnNames.COUNTRY and row[column_name.value] is not Regulation.NO_DATA])
+
+gun_laws_df.apply(calculate_mean_regulation, axis=1)
 
 print(gun_laws_df.head())
