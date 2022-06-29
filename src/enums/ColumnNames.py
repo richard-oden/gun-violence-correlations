c5f3acdbe3c7e8f8583434d2e02477c22703f696
Richard Oden <richard.thomas.oden@gmail.com>
2022-06-29 19:45:35 -0400

    Added regulation_column_names in ColumnNames.py

--- src/enums/ColumnNames.py
+++ src/enums/ColumnNames.py
@@ -16,3 +16,15 @@ class ColumnNames(enum.Enum):
     FREE_OF_REGISTRATION = 'Free of Registration'
     DEATH_RATE = 'Death Rate by Firearm per 100k Citizens'
     OVERALL_REGULATION = 'Overall Regulation Score'
+
+regulation_column_names = [
+    ColumnNames.GOOD_REASON,
+    ColumnNames.PERSONAL_PROTECTION,
+    ColumnNames.LONG_GUNS,
+    ColumnNames.HANDGUNS,
+    ColumnNames.SEMIAUTOMATIC,
+    ColumnNames.FULLY_AUTOMATIC,
+    ColumnNames.OPEN_CARRY,
+    ColumnNames.CONCEALED_CARRY,
+    ColumnNames.FREE_OF_REGISTRATION
+]
