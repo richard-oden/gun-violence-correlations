c782919d24ca62b8d9e0fb3e2b4de1331b8fe17e
Richard Oden <richard.thomas.oden@gmail.com>
2022-06-29 19:41:36 -0400

    Added ColumnNames.py in enums module

---
+++ src/enums/ColumnNames.py
@@ -0,0 +1,18 @@
+import enum
+
+class ColumnNames(enum.Enum):
+    '''
+    Represents column names used by dataframes.
+    '''
+    COUNTRY = 'Country'
+    GOOD_REASON = 'Good Reason Required'
+    PERSONAL_PROTECTION = 'Personal Protection Permitted'
+    LONG_GUNS = 'Long Guns Permitted'
+    HANDGUNS = 'Handguns Permitted'
+    SEMIAUTOMATIC = 'Semi-Automatic Permitted'
+    FULLY_AUTOMATIC = 'Fully Automatic Permitted'
+    OPEN_CARRY = 'Open Carry Permitted'
+    CONCEALED_CARRY = 'Concealed Carry Permitted'
+    FREE_OF_REGISTRATION = 'Free of Registration'
+    DEATH_RATE = 'Death Rate by Firearm per 100k Citizens'
+    OVERALL_REGULATION = 'Overall Regulation Score'
--- src/enums/Regulations.py
+++ src/enums/Regulations.py
@@ -1,6 +1,10 @@
 import enum
 
 class Regulation(enum.Enum):
+    '''
+    An enum representing how regulated a particular aspect of gun legislation is.
+    Backing value is an int, which may be used to calulcate average regulation.
+    '''
     NO_DATA = -1
     HIGHLY_REGULATED = 0
     MOSTLY_REGULATED = 1
