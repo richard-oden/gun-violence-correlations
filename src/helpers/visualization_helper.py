d67f5b8d1ae1e80704a9cd61beee6ff087fb4d58
Richard Oden <richard.thomas.oden@gmail.com>
2022-07-02 11:26:08 -0400

    Added bokeh demo in visualization_helper using project data.

--- src/helpers/visualization_helper.py
+++ src/helpers/visualization_helper.py
@@ -1,20 +1,38 @@
 import os
+import pandas as pd
 from bokeh.plotting import figure, output_file, show
+from bokeh.models import ColumnDataSource
+from bokeh.models.tools import HoverTool
+from enums.ColumnName import ColumnName, REGULATION_COLUMN_NAMES
 
-if not os.path.exists('output'):
-    os.makedirs('output')
 
-output_file(os.path.join('output', 'visualization.html'))
+def initialize_output() -> None:
+    '''
+    Defines the default output file for bokeh as `output/visualization.html`.
+    '''
+    if not os.path.exists('output'):
+        os.makedirs('output')
 
-x = [1, 3, 5, 7]
-y = [2, 4, 6, 8]
+    output_file(os.path.join('output', 'visualization.html'))
 
-p = figure()
+def create_plot(df: pd.DataFrame):
+    '''
+    Creates bokeh `Figure` object using the supplied `DataFrame` and displays it. 
+    '''
+    column_data_source = ColumnDataSource(df)
+    fig = figure(plot_width=1400, x_range=list(df[ColumnName.COUNTRY.value].unique()))
+    fig.circle(x=ColumnName.COUNTRY.value, y=ColumnName.DEATH_RATE.value,
+         source=column_data_source,
+         size=10, color='green')
+    fig.title.text = 'Firearm-Related Death Rate per 100,000 Citizens by Country'
+    fig.xaxis.axis_label = ColumnName.COUNTRY.value
+    fig.yaxis.axis_label = ColumnName.DEATH_RATE.value
+    fig.xaxis.major_label_orientation = 'vertical'
 
-p.circle(x, y, size=10, color='red', legend='circle')
-p.line(x, y, color='blue', legend='line')
-p.triangle(y, x, color='gold', size=10, legend='triangle')
+    hover_tool = HoverTool()
+    hover_tool.tooltips = [
+        (ColumnName.OVERALL_REGULATION.value, f'@{{{ColumnName.OVERALL_REGULATION.value}}}')
+    ]
+    fig.add_tools(hover_tool)
 
-p.legend.click_policy='hide'
-
-show(p)
+    show(fig)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import helpers.data_helper as dh
 import helpers.visualization_helper as vh
 
-# df = dh.get_cleaned_data()
-
-# print(df)
+df = dh.get_cleaned_data()
+vh.initialize_output()
+vh.create_plot(df)
