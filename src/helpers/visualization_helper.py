78aeefdbf63c56025e08bf9ba1b00549d7af4fbf
Richard Oden <richard.thomas.oden@gmail.com>
2022-07-09 19:12:33 -0400

    create_plot and create_regression_line now accept x_column_name and y_column_name.

--- src/helpers/visualization_helper.py
+++ src/helpers/visualization_helper.py
@@ -1,8 +1,9 @@
 import os
 import numpy as np
 import pandas as pd
+from bokeh.io import show
 from bokeh.plotting import figure, output_file, show
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, CustomJS, Select
 from bokeh.models.tools import HoverTool
 from enums.ColumnName import ColumnName, TEXT_COLUMN_NAMES
 
@@ -16,7 +17,7 @@ def initialize_output() -> None:
 
     output_file(os.path.join('output', 'visualization.html'))
 
-def create_regression_line(df: pd.DataFrame) -> list:
+def create_regression_line(df: pd.DataFrame, x_column_name: str, y_column_name: str) -> list:
     '''
     Given the supplied `DataFrame`, calculates a regression line indicating the
     overall trend.
@@ -24,34 +25,41 @@ def create_regression_line(df: pd.DataFrame) -> list:
     Parameters
     ---
     `df` : `DataFrame` object
+    `x_column_name` : `str` representing the column to be plotted on the x-axis
+    `y_column_name` : `str` representing the column to be plotted on the y-axis
 
     Returns
     ---
     a `list` of y coordinate values for the regression line.
     '''
-    par = np.polyfit(df[ColumnName.OVERALL_REGULATION.value], df[ColumnName.DEATH_RATE.value], 1, full=True)
+    par = np.polyfit(df[x_column_name], df[y_column_name], 1, full=True)
     slope=par[0][0]
     intercept=par[0][1]
-    return [slope*x + intercept for x in df[ColumnName.OVERALL_REGULATION.value]]
 
-def create_plot(df: pd.DataFrame) -> None:
+    return [slope*x + intercept for x in df[x_column_name]]
+
+def create_plot(df: pd.DataFrame, x_column_name: str, y_column_name: str) -> None:
     '''
     Creates bokeh `Figure` object using the supplied `DataFrame` and displays it.
 
     Parameters
     ---
     `df` : `DataFrame` object
+    `x_column_name` : `str` representing the column to be plotted on the x-axis
+    `y_column_name` : `str` representing the column to be plotted on the y-axis
     '''
-    column_data_source = ColumnDataSource(df)
+    relevant_df = df.dropna(subset=[x_column_name, y_column_name]).drop(df[(df[x_column_name] == -1) | (df[y_column_name] == -1)].index)
+
+    column_data_source = ColumnDataSource(relevant_df)
     fig = figure(plot_width=1000)
-    fig.circle(x=ColumnName.OVERALL_REGULATION.value, y=ColumnName.DEATH_RATE.value,
+    fig.circle(x=x_column_name, y=y_column_name,
         source=column_data_source, size=8, color='black')
-    fig.line(x=df[ColumnName.OVERALL_REGULATION.value], y=create_regression_line(df),
+    fig.line(x=relevant_df[x_column_name], y=create_regression_line(relevant_df, x_column_name, y_column_name),
         color='red')
 
-    fig.title.text = 'Firearm-Related Death Rate vs Overall Firearm Regulation'
-    fig.xaxis.axis_label = ColumnName.OVERALL_REGULATION.value
-    fig.yaxis.axis_label = ColumnName.DEATH_RATE.value
+    fig.title.text = f'{x_column_name} vs {y_column_name}'
+    fig.xaxis.axis_label = x_column_name
+    fig.yaxis.axis_label = y_column_name
 
     tooltip_columns = [ColumnName.COUNTRY, *TEXT_COLUMN_NAMES, ColumnName.CIVILIAN_FIREARMS, ColumnName.MILITARY_FIREARMS, ColumnName.POLICE_FIREARMS]
 
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import helpers.data_helper as dh
 import helpers.visualization_helper as vh
+from enums.ColumnName import ColumnName
 
 df = dh.get_cleaned_data()
 vh.initialize_output()
-vh.create_plot(df)
+vh.create_plot(df, ColumnName.CIVILIAN_FIREARMS.value, ColumnName.DEATH_RATE.value)
