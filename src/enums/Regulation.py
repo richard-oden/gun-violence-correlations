f37cbcfed462e5932def1cd7537c69b369dbffed
Richard Oden <richard.thomas.oden@gmail.com>
2022-07-17 11:43:45 -0400

    Regulation scores are from 0-100 instead of 0-4.

--- src/enums/Regulation.py
+++ src/enums/Regulation.py
@@ -6,8 +6,8 @@ class Regulation(enum.Enum):
     Backing value is an int, which may be used to calulcate average regulation.
     '''
     NO_DATA = -1
-    HIGHLY_REGULATED = 4
-    MOSTLY_REGULATED = 3
-    CONDITIONAL = 2
-    MOSTLY_UNREGULATED = 1
+    HIGHLY_REGULATED = 100
+    MOSTLY_REGULATED = 75
+    CONDITIONAL = 50
+    MOSTLY_UNREGULATED = 25
     HIGHLY_UNREGULATED = 0
