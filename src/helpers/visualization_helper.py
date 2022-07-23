c2e754ae95c4daf2f08e365a19cdaf6b56288d06
Richard Oden <richard.thomas.oden@gmail.com>
2022-07-23 16:06:46 -0400

    Added ability to highlight a specific country in bokeh visualization.

--- src/helpers/visualization_helper.py
+++ src/helpers/visualization_helper.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from bokeh.layouts import column, row
 from bokeh.plotting import figure, output_file, curdoc
-from bokeh.models import ColumnDataSource, CustomJS, Select
+from bokeh.models import ColumnDataSource, Circle, Select
 from bokeh.models.tools import HoverTool
 from enums.ColumnName import REGULATION_COLUMN_NAMES, ColumnName, TEXT_COLUMN_NAMES
 
@@ -31,7 +31,7 @@ def create_regression_line(df: pd.DataFrame, x_column_name: str, y_column_name: 
     return [slope*x + intercept for x in df[x_column_name]]
 
 
-def create_plot(df: pd.DataFrame, x_column_name: str, y_column_name: str) -> Figure:
+def create_plot(df: pd.DataFrame, x_column_name: str, y_column_name: str, highlighted_country_name: str) -> Figure:
     '''
     Creates bokeh `Figure` object using the supplied `DataFrame`.
 
@@ -40,6 +40,7 @@ def create_plot(df: pd.DataFrame, x_column_name: str, y_column_name: str) -> Fig
     `df` : `DataFrame` object
     `x_column_name` : `str` representing the column to be plotted on the x-axis
     `y_column_name` : `str` representing the column to be plotted on the y-axis
+    `highlighted_country_name` : `str` name of the country to be highlighted in the plot
 
     Returns
     ---
@@ -51,11 +52,17 @@ def create_plot(df: pd.DataFrame, x_column_name: str, y_column_name: str) -> Fig
 
     # Fill cells where where data could not be found with human-friendly string for tooltips.
     relevant_df = relevant_df.fillna('No data found').replace([-1], 'No data found')
+    highlighted_country_df = relevant_df[relevant_df[ColumnName.COUNTRY.value] == highlighted_country_name]
 
-    column_data_source = ColumnDataSource(relevant_df)
+    column_data_source = ColumnDataSource(relevant_df.drop(highlighted_country_df.index))
+    highlighted_column_data_source = ColumnDataSource(highlighted_country_df)
+
     fig = figure(plot_width=1000)
     fig.circle(x=x_column_name, y=y_column_name,
-        source=column_data_source, size=10, line_color='white', color='#2F2F2F', hover_color='white', alpha=0.6, hover_alpha=1, name='countries')
+        source=column_data_source, size=10, color="#2F2F2F", line_color='white', alpha=0.5, hover_alpha=1, hover_color='white', name='countries')
+    fig.circle(x=x_column_name, y=y_column_name,
+        source=highlighted_column_data_source, size=10, color="red", name='highlighted country')
+
     fig.line(x=relevant_df[x_column_name], y=create_regression_line(relevant_df, x_column_name, y_column_name),
         color='red')
 
@@ -65,7 +72,7 @@ def create_plot(df: pd.DataFrame, x_column_name: str, y_column_name: str) -> Fig
 
     tooltip_columns = [ColumnName.COUNTRY, *TEXT_COLUMN_NAMES, ColumnName.CIVILIAN_FIREARMS, ColumnName.MILITARY_FIREARMS, ColumnName.POLICE_FIREARMS]
 
-    hover_tool = HoverTool(tooltips=[(column_name.value, f'@{{{column_name.value}}}') for column_name in tooltip_columns], names=['countries'])
+    hover_tool = HoverTool(tooltips=[(column_name.value, f'@{{{column_name.value}}}') for column_name in tooltip_columns], names=['countries', 'highlighted country'])
     fig.add_tools(hover_tool)
 
     return fig
@@ -80,17 +87,20 @@ def initialize_bokeh(df: pd.DataFrame):
         ColumnName.MILITARY_FIREARMS, 
         ColumnName.POLICE_FIREARMS]]
 
+    countries = df[ColumnName.COUNTRY.value].tolist()
+
     x_select = Select(title='X-Axis Statistic', value=ColumnName.OVERALL_REGULATION.value, options=selectable_columns)
     y_select = Select(title='Y-Axis Statistic', value=ColumnName.DEATH_RATE.value, options=selectable_columns)
+    highlighted_country_select = Select(title='Highlighted Country', value='United States', options=countries)
 
-    controls = column(x_select, y_select, width=400)
-    layout = row(controls, create_plot(df, x_select.value, y_select.value))
+    controls = column(x_select, y_select, highlighted_country_select, width=400)
+    layout = row(controls, create_plot(df, x_select.value, y_select.value, highlighted_country_select.value))
 
     #on_change callback functions must have the signature func(attr, old, new).
     def update(attr, old, new):
-        layout.children[1] = create_plot(df, x_select.value, y_select.value)
+        layout.children[1] = create_plot(df, x_select.value, y_select.value, highlighted_country_select.value)
 
-    [select.on_change('value', update) for select in [x_select, y_select]]
+    [select.on_change('value', update) for select in [x_select, y_select, highlighted_country_select]]
 
     curdoc().add_root(layout)
     curdoc().title = "Gun Violence Correlations"
