de3c25a35c9479488f0ddf149d9bbb7274fccfd3
Richard Oden <richard.thomas.oden@gmail.com>
2022-07-24 12:02:10 -0400

    Added docstrings in log_helper.

--- src/helpers/log_helper.py
+++ src/helpers/log_helper.py
@@ -4,6 +4,9 @@ import time
 from logging.handlers import TimedRotatingFileHandler
 
 def configure_logging() -> None:
+    '''
+    Configures logging for the application. Logs are divided into separate files by day and saved in /static/logs.
+    '''
     logging.basicConfig(
         format='%(asctime)s -- [%(levelname)s]: %(message)s',
         datefmt='%m/%d/%Y %I:%M:%S %p',
@@ -18,15 +21,36 @@ def configure_logging() -> None:
 
 
 def log_info(message: str) -> None:
+    '''
+    Logs a message with severity INFO.
+
+    Parameters
+    ---
+    `message` : `str` representing the message to be logged.
+    '''
     logging.info(message)
 
 def log_error(message: str) -> None:
+    '''
+    Logs a message with severity ERROR.
+
+    Parameters
+    ---
+    `message` : `str` representing the message to be logged.
+    '''
     logging.error(message)
 
 def start_timed_log(message: str) -> float:
+    '''
+    Logs a message with severity INFO and returns.
+
+    Parameters
+    ---
+    `message` : `str` representing the message to be logged.
+    '''
     logging.info(message)
     return time.time()
 
-def stop_timed_log(message: str, start_time: float) -> None:
+def stop_timed_log(message: str, start_time: float) -> float:
     logging.info(message)
     return round((time.time() - start_time), 2)
