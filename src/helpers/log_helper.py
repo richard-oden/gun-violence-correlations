92f47f02ac4a4984b9cabb1b4d959953d430eab4
Richard Oden <richard.thomas.oden@gmail.com>
2022-07-24 11:53:36 -0400

    Added helper methods to log_helper.py.

--- src/helpers/log_helper.py
+++ src/helpers/log_helper.py
@@ -1,8 +1,9 @@
 import logging
 import os
+import time
 from logging.handlers import TimedRotatingFileHandler
 
-def configure_logging():
+def configure_logging() -> None:
     logging.basicConfig(
         format='%(asctime)s -- [%(levelname)s]: %(message)s',
         datefmt='%m/%d/%Y %I:%M:%S %p',
@@ -14,3 +15,18 @@ def configure_logging():
     handler.suffix = "%Y-%m-%d"
 
     logging.getLogger().addHandler(handler)
+
+
+def log_info(message: str) -> None:
+    logging.info(message)
+
+def log_error(message: str) -> None:
+    logging.error(message)
+
+def start_timed_log(message: str) -> float:
+    logging.info(message)
+    return time.time()
+
+def stop_timed_log(message: str, start_time: float) -> None:
+    logging.info(message)
+    return round((time.time() - start_time), 2)
